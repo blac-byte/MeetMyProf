13e3130adc168af9bb4a157bf36d720b729b3bb9
blac-byte <zabook65@gmail.com>
2025-10-21 19:33:48 +0530

    .

--- app/services/parser.py
+++ app/services/parser.py
@@ -3,7 +3,7 @@
 from flask import request, Blueprint, url_for, redirect, render_template, flash
 from sqlalchemy.exc import IntegrityError, SQLAlchemyError
 from flask_login import login_required, current_user
-from ..models import Classes
+from ..models import Classes, Time
 from app import db
 from ..utils.errors import register_error_handlers
 
@@ -24,9 +24,13 @@ def parser_check():
     
     if request.method=='POST':
         user_id=current_user.get_id()
+        theory_timing, lab_timing=[],[]
         theory={}
         lab={}
         
+        theory_start, theory_end = None, None
+        lab_start, lab_end = None, None
+
         # gets the copy-pasted timetable
         raw_text=request.form.get('table')
         rows=raw_text.splitlines()
@@ -34,10 +38,28 @@ def parser_check():
         day=None
         for row in rows:
             part=row.split('\t')
+
+            if part[0]=='THEORY' and part[1]=='Start':
+                 # ['08:00', '09:00', '10:00', '11:00', '12:00', '-', 'Lunch', '14:00', '15:00', '16:00', '17:00', '18:00', '18:51', '19:01']
+                 theory_start=part[2:]
+
+            elif part[0]=='End' and not theory_end:
+                 # ['08:50', '09:50', '10:50', '11:50', '12:50', '-', 'Lunch', '14:50', '15:50', '16:50', '17:50', '18:50', '19:00', '19:50']
+                 theory_end=part[1:]
+
+            elif part[0]=='LAB' and part[1]=='Start':
+                 # ['Start', '08:00', '08:51', '09:51', '10:41', '11:40', '12:31', 'Lunch', '14:00', '14:51', '15:51', '16:41', '17:40', '18:31', '-']
+                 lab_start=part[2:]
+
+            elif part[0]=='End' and not lab_end:
+                 # ['08:50', '09:40', '10:40', '11:30', '12:30', '13:20', 'Lunch', '14:50', '15:40', '16:40', '17:30', '18:30', '19:20', '-']
+                 lab_end=part[1:]
+
+
             # Only takes the class slots, ignoring the theory and lab time slot strips
             # since they are universal and admin controlled db table
             
-            if part[0].strip() in ['MON','TUE','WED','THU','FRI','SAT','SUN']:
+            elif part[0].strip() in ['MON','TUE','WED','THU','FRI','SAT','SUN']:
                  day=part[0].strip()
                  theory[day] = [item.strip() for item in part[2:] if item.strip() != 'Lunch']
                  # ['A1', 'F1', 'D1', 'TB1', 'TG1', '-', 'Lunch', 'A2-BAEEE101-ETH-PRP105-ALL03', 'F2', 'D2-BACSE103-ETH-PRP105-ALL03', 'TB2-BAMAT101-ETH-PRP105-ALL03', 'TG2-BACHY105-ETH-PRP105-ALL03', '-', 'V3']
@@ -58,24 +80,25 @@ def parser_check():
                  # ['L71', 'L72', 'L73', 'L74', 'L75', 'L76', 'Lunch', 'L77', 'L78', 'L79', 'L80', 'L81', 'L82', '-']
                  # ['L83', 'L84', 'L85', 'L86', 'L87', 'L88', 'Lunch', 'L89', 'L90', 'L91', 'L92', 'L93', 'L94', '-']
 
-     #    column_id=0
-     #    try:
-     #      for slot_theory, slot_lab in zip(theory_timing,lab_timing):
-     #           db.session.add(Time(user_id, column_id, slot_theory['start'], slot_theory['end'], 'ETH'))
-     #           db.session.add(Time(user_id, column_id, slot_lab['start'], slot_lab['end'], 'ELA'))
-     #           column_id+=1
+      
+        try:
+          for column_id, (slot_theory, slot_lab) in zip(theory_timing,lab_timing):
+               db.session.add(Time(user_id, column_id, slot_theory['start'], slot_theory['end'], 'ETH'))
+               db.session.add(Time(user_id, column_id, slot_lab['start'], slot_lab['end'], 'ELA'))
+          db.session.commit()
 
-     #    except SQLAlchemyError as e:
-     #  # Rollback for other database errors
-     #      db.session.rollback()
-     #      flash(f'An error occurred: {str(e)}')
-     #      register_error_handlers.server_error()
 
-     #    except IntegrityError:
-     #      # Rollback session in case of database integrity error
-     #      db.session.rollback()
-     #      flash('Integrity error occurred. Please check your data.')
-     #      register_error_handlers.server_error()
+        except SQLAlchemyError as e:
+      # Rollback for other database errors
+          db.session.rollback()
+          flash(f'An error occurred: {str(e)}')
+          return register_error_handlers.server_error()
+
+        except IntegrityError:
+          # Rollback session in case of database integrity error
+          db.session.rollback()
+          flash('Integrity error occurred. Please check your data.')
+          register_error_handlers.server_error()
 #-------------------------------------------------------------------------------------
         commit_log=set()
         
